caa7f265e92f7aeb71441dc35b77e61998738d4c
Iris <therebecode@gmail.com>
2020-11-15 20:26:25 +0200

    change background gradient with two color-pickers

---
+++ script.js
@@ -0,0 +1,11 @@
+var body = document.getElementById("gradient");
+var color1 = document.querySelector("#color1");
+var color2 = document.querySelector("#color2");
+
+function setGradient(){
+  body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
